abc6ed1d3a6f6755948ada8eea0bd67b5be7fed5
sang hyeon <ppsh0703@daum.net>
2024-05-07 01:48:05 +0900

    셀레니움 공부중

--- view tracking/app.py
+++ view tracking/app.py
@@ -10,11 +10,8 @@ options.add_experimental_option("detach",True)
 chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome(options)
-driver.get("https://www.naver.com")
-time.sleep(3)
-css_selector ="#shortcutArea > ul > li:nth-child(4)"
-그룹_네비게이션 = driver.find_element(By.CSS_SELECTOR, css_selector)
 
-print(그룹_네비게이션.text)
-그룹_네비게이션.click()
+query="python flask"
+search_link = f"https://search.naver.com/search.naver?ssc=tab.cafe.all&sm=tab_jum&query={query}"
+driver.get(search_link)
 input()
